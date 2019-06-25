7f1f354db85002c24ba3edbc55760c36bfefbbd4
MechaelDodo <modod@bk.ru>
2019-06-25 19:23:31 +0300

    create class 'Convert'

--- math_modules/mathconvert.py
+++ math_modules/mathconvert.py
@@ -1,78 +1,94 @@
 from .mathsigns import TOUPLE_COUNST, DICT_MATH, TOUPLE_COUNST_inf_nan
 
-def convertdigit(list_expr):        #returns new list(doesn't change list): ['3','3'] => ['33']
-    index = 0
-    digitlist = []
-    list_expr_res = []
-    while True:
-        if index == len(list_expr):
-            digitlist = ''.join(digitlist)
-            list_expr_res.append(digitlist)
-            index += 1
-            digitlist = []
-            break
-        if not str(list_expr[index]).isdigit() and bool(digitlist):           
-            digitlist = ''.join(digitlist)
-            list_expr_res.append(digitlist)
-            list_expr_res.append(list_expr[index])
-            index += 1
-            digitlist = []
-        elif not str(list_expr[index]).isdigit() and not digitlist:
-            list_expr_res.append(list_expr[index])
-            index += 1
-        else:
-            digitlist.append(list_expr[index])
-            index += 1
-    return list_expr_res
+class Convert:
+
+    def __init__(self, list_expr):
+        self.list_expression = list_expr
+    
+    def convert_digit(self):        #returns new list(doesn't change list): ['3','3'] => ['33']
+        index = 0
+        digitlist = []
+        list_expr_res = []
+        while True:
+            if index == len(self.list_expression):
+                digitlist = ''.join(digitlist)
+                list_expr_res.append(digitlist)
+                index += 1
+                digitlist = []
+                break
+            if not str(self.list_expression[index]).isdigit() and bool(digitlist):           
+                digitlist = ''.join(digitlist)
+                list_expr_res.append(digitlist)
+                list_expr_res.append(self.list_expression[index])
+                index += 1
+                digitlist = []
+            elif not str(self.list_expression[index]).isdigit() and not digitlist:
+                list_expr_res.append(self.list_expression[index])
+                index += 1
+            else:
+                digitlist.append(self.list_expression[index])
+                index += 1
+        self.list_expression = list_expr_res
 
 
-def convertfloat(list_expr):        #changes list: ['2', '.', '3'] => ['2.3']
-    for offset, num in enumerate(list_expr[:]):            
-        if num == '.' and list_expr[offset-1].isdigit() and offset != 0 and list_expr[offset+1].isdigit():
-            floatnum = ''.join(list_expr[offset-1:offset+2])
-            del list_expr[offset-1:offset+2]
-            list_expr.insert(offset-1, floatnum)
-            return convertfloat(list_expr)
-        if num == '.' and not list_expr[offset+1].isdigit():
-            list_expr.insert(offset+1, '0')
-            floatnum = ''.join(list_expr[offset-1:offset+2])
-            del list_expr[offset-1:offset+2]
-            list_expr.insert(offset-1, floatnum)
-            return convertfloat(list_expr)
-        elif (num == '.' and offset == 0) or num == '.':
-            list_expr.insert(offset, '0')
-            floatnum = ''.join(list_expr[offset:offset+3])
-            del list_expr[offset:offset+3]
-            list_expr.insert(offset, floatnum)
-            return convertfloat(list_expr)
+    def convert_float(self):        #changes list: ['2', '.', '3'] => ['2.3']
+        for offset, num in enumerate(self.list_expression[:]):            
+            if (num == '.' and self.list_expression[offset-1].isdigit() and
+                offset != 0 and
+                self.list_expression[offset+1].isdigit()):
+                floatnum = ''.join(self.list_expression[offset-1:offset+2])
+                del self.list_expression[offset-1:offset+2]
+                self.list_expression.insert(offset-1, floatnum)
+                return self.convert_float()
+            if num == '.' and not self.list_expression[offset+1].isdigit():
+                self.list_expression.insert(offset+1, '0')
+                floatnum = ''.join(self.list_expression[offset-1:offset+2])
+                del self.list_expression[offset-1:offset+2]
+                self.list_expression.insert(offset-1, floatnum)
+                return self.convert_float()
+            elif (num == '.' and offset == 0) or num == '.':
+                self.list_expression.insert(offset, '0')
+                floatnum = ''.join(self.list_expression[offset:offset+3])
+                del self.list_expression[offset:offset+3]
+                self.list_expression.insert(offset, floatnum)
+                return self.convert_float()
 
-def convertconst(list_expr):        #['e'] => ['2.718281828459045']
-    for offset, num in enumerate(list_expr):
-        if num in TOUPLE_COUNST:
-            resultnum = str(DICT_MATH[num])
-            del list_expr[offset]
-            list_expr.insert(offset, resultnum)
-            return convertconst(list_expr)
-        elif num in TOUPLE_COUNST_inf_nan:
-            list_expr[offset] = float(num)
+    def convert_const(self):        #['e'] => ['2.718281828459045']
+        for offset, num in enumerate(self.list_expression):
+            if num in TOUPLE_COUNST:
+                resultnum = str(DICT_MATH[num])
+                del self.list_expression[offset]
+                self.list_expression.insert(offset, resultnum)
+                return self.convert_const()
+            elif num in TOUPLE_COUNST_inf_nan:
+                self.list_expression[offset] = float(num)
 
-def normalization(list_expr):
-    res = list_expr[0]
-    resoff = list(res)
-    tail = None
-    for offset, num in enumerate(resoff):
-        if num == '.':                
-            tail = resoff[offset+1:]
-            break
-    sumtail = 0
-    if tail is not None:
-        for num in tail:
-            sumtail += int(num)
-    if sumtail == 0:
-        res = int(float(res))
-    return res
-        
+    def normalization(list_expr):
+        res = list_expr[0]
+        resoff = list(res)
+        tail = None
+        for offset, num in enumerate(resoff):
+            if num == '.':                
+                tail = resoff[offset+1:]
+                break
+        sumtail = 0
+        if tail is not None:
+            for num in tail:
+                sumtail += int(num)
+        if sumtail == 0:
+            res = int(float(res))
+        return res
+            
+if __name__ == '__main__':
+    c = Convert(['3', '3', '+', '2', '.', '3','+', 'e'])
+    c.convert_digit()
+    c.convert_float()
+    c.convert_const()
+    print(c.list_expression)
 
+    a = ['2.0']
+    a = Convert.normalization(a)
+    print(a)
 
                       
 
