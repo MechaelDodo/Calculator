6f52cf4fec3fd721f6ee0ddc12e429ecd0f488ae
MechaelDodo <modod@bk.ru>
2019-06-25 19:06:38 +0300

    make class 'Transformer'

--- math_modules/mathtransform.py
+++ math_modules/mathtransform.py
@@ -1,67 +1,73 @@
 from .mathsigns import *
 
-def signtransformation(list_expr):      #['2', '<', '=', '3'] => ['2', '<=', 3]
-    for offset, num in enumerate(list_expr[:]):
-        if offset == 0: continue
-        
-        sign = str(list_expr[offset-1]) + str(num)        
-        if sign in DICT_MUL_DIV or sign in BOOL_DICT:
-            del list_expr[offset-1:offset+1]
-            list_expr.insert(offset-1, sign)
-            signtransformation(list_expr)
-    return list_expr
+class Transformer:
 
+    def __init__(self, list_expr):
+        self.list_expression = list_expr
+    
+    def sign_transformation(self):      #['2', '<', '=', '3'] => ['2', '<=', 3]
+        for offset, num in enumerate(self.list_expression[:]):
+            if offset == 0: continue        
+            sign = str(self.list_expression[offset-1]) + str(num)        
+            if sign in DICT_MUL_DIV or sign in BOOL_DICT:
+                del self.list_expression[offset-1:offset+1]
+                self.list_expression.insert(offset-1, sign)
+                self.sign_transformation()
 
+    def sub_transformation(self):       #['-', '5', '+', '1'] => ['-5', '+', '1']
+        if self.list_expression[0] in DICT_ADD_SUB:
+            num = self.list_expression[0] + self.list_expression[1]
+            del self.list_expression[0:2]
+            self.list_expression.insert(0, num)
 
-def subtransformation(list_expr):       #['-', '5', '+', '1'] => ['-5', '+', '1']
-    if list_expr[0] in DICT_ADD_SUB:
-        num = list_expr[0] + list_expr[1]
-        del list_expr[0:2]
-        list_expr.insert(0, num)
-        return list_expr
-    else:
-        return list_expr
+    def space_transformation(self):     #deletes all spaces
+        for offset, num in enumerate(self.list_expression[:]):
+            if num == ' ':
+                del self.list_expression[offset]
+                self.space_transformation()
 
-def spacetransformation(list_expr):     #deletes all spaces
-    for offset, num in enumerate(list_expr[:]):
-        if num == ' ':
-            del list_expr[offset]
-            return (spacetransformation(list_expr))
-        
-
-def mathtransformation(list_expr):      #['a', 'b', 's', '(', '2', '-', '10', ')'] => ['abs', '(', '2', '-', '10', ')']
-    firstoffset = 0
-    secondoffset = 0
-    list_expr.append('')                #QUICK'N'DIRTY      
-    while True:
-        checkvalue = ''.join(list_expr[firstoffset:secondoffset])
-        if (checkvalue in DICT_MATH_ARGS or
-            checkvalue in DICT_ROUND or
-            checkvalue in DICT_ABS or
-            checkvalue in DICT_MATH or
-            checkvalue in TOUPLE_COUNST_inf_nan):
-            if checkvalue + list_expr[secondoffset] != 'atan2':
-                del list_expr[firstoffset:secondoffset]
-                list_expr.insert(firstoffset, checkvalue)
-                secondoffset -= len(checkvalue) #+1          
+    def math_transformation(self):      #['a', 'b', 's', '(', '2', '-', '10', ')'] => ['abs', '(', '2', '-', '10', ')']
+        firstoffset = 0
+        secondoffset = 0
+        self.list_expression.append('')                 #QUICK'N'DIRTY      
+        while True:
+            checkvalue = ''.join(self.list_expression[firstoffset:secondoffset])
+            if (checkvalue in DICT_MATH_ARGS or
+                checkvalue in DICT_ROUND or
+                checkvalue in DICT_ABS or
+                checkvalue in DICT_MATH or
+                checkvalue in TOUPLE_COUNST_inf_nan):
+                if checkvalue + self.list_expression[secondoffset] != 'atan2':
+                    del self.list_expression[firstoffset:secondoffset]
+                    self.list_expression.insert(firstoffset, checkvalue)
+                    secondoffset -= len(checkvalue) #+1          
+                    firstoffset += 1
+                else:
+                    checkvalue = checkvalue + self.list_expression[secondoffset]
+                    del self.list_expression[firstoffset:secondoffset+1]
+                    self.list_expression.insert(firstoffset, checkvalue)
+                    secondoffset -= len(checkvalue)
+                    firstoffset += 1
+            elif secondoffset == len(self.list_expression):
+                secondoffset = firstoffset+2
                 firstoffset += 1
+            elif firstoffset == len(self.list_expression):
+                break
             else:
-                checkvalue = checkvalue + list_expr[secondoffset]
-                del list_expr[firstoffset:secondoffset+1]
-                list_expr.insert(firstoffset, checkvalue)
-                secondoffset -= len(checkvalue)
-                firstoffset += 1
-        elif secondoffset == len(list_expr):
-            secondoffset = firstoffset+2
-            firstoffset += 1
-        elif firstoffset == len(list_expr):
-            break
-        else:
-            secondoffset += 1
-    return list_expr
+                secondoffset += 1
 
-def delete_empty(list_expr):
-    for offset, num in enumerate(list_expr):
-        if num == '':
-            del list_expr[offset]
-            return delete_empty(list_expr)
+    def delete_empty(list_expr):
+        for offset, num in enumerate(list_expr):
+            if num == '':
+                del list_expr[offset]
+                return delete_empty(list_expr)
+
+if __name__ == '__main__':
+    t = Transformer(['-', '5', '+',' ', '1', '<', '=', '3', '+','a', 'b', 's', '(', '2', '-', '10', ')'])
+    t.sign_transformation()
+    t.sub_transformation()
+    t.space_transformation()
+    t.math_transformation()
+    print(t.list_expression)
+        
+
